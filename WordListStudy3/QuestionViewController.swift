53ab4f31c5cf8c131cb4bc62b8bfc56875675355
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-05-16 19:23:47 +0900

    1st

---
+++ WordListStudy3/QuestionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuestionViewController.swift
+//  WordListStudy3
+//
+//  Created by Koki Ide on 2017/05/16.
+//  Copyright © 2017 kokiide. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

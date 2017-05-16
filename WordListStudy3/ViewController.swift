1b0d5e9dd2c096d1279513edd80b0a9352e38d5d
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-05-16 18:39:07 +0900

    Initial Commit

---
+++ WordListStudy3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordListStudy3
+//
+//  Created by Koki Ide on 2017/05/16.
+//  Copyright © 2017 kokiide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WordListStudy3Tests/WordListStudy3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  WordListStudy3Tests.swift
+//  WordListStudy3Tests
+//
+//  Created by Koki Ide on 2017/05/16.
+//  Copyright © 2017 kokiide. All rights reserved.
+//
+
+import XCTest
+@testable import WordListStudy3
+
+class WordListStudy3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
